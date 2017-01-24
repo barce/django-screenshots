bde03cb1dce24d85393489fda89b273e99974769
Barce <james.barcelona@accuenmedia.com>
2017-01-24 15:06:45 -0800

    updating dependencies to include future package

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-screenshots',
-    version='0.1.3',
+    version='0.1.5',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['django_screenshots', 'django_screenshots.tests'],
+    install_requires=[
+      'future',
+    ],
     scripts=[],
     url='http://pypi.python.org/pypi/django-screenshots/',
     license='LICENSE.txt',
