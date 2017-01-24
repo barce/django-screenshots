3140da7e3596f2b05c0d55f189f7f623a8d3f0ba
Barce <james.barcelona@accuenmedia.com>
2017-01-24 15:10:14 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-screenshots',
-    version='0.1.5',
+    version='0.1.6',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['django_screenshots', 'django_screenshots.tests'],
